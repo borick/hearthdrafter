be0fd3abbc68963bd321b94ca39d46304e08b82a
Boris Sitsker <boris@sitsker.com>
2015-02-05 00:34:22 -0500

    changed schema, denormalized it.

--- install/utils/card_loader.pl
+++ install/utils/card_loader.pl
@@ -22,10 +22,9 @@ $ds->connect->get;
 my $debug = 2;
 
 my $card_data_file = 'data/AllSets.json';
-my $card_data_text = read_file($card_data_file);
-my $cards = decode_json $card_data_text;
-my @sets_to_load = ('Basic', 'Classic', 'Curse of Naxxramas', 'Goblins vs Gnomes');
-my $counter = 0;
+my $ha_data_folder = 'ha_tier_data';
+my @files = glob("$ha_data_folder/*.txt"); #json files
+my %class_maps = ();
 my %class_ids = (1 => 'druid',
                  2 => 'hunter',
                  3 => 'mage',
@@ -35,6 +34,31 @@ my %class_ids = (1 => 'druid',
                  7 => 'shaman',
                  8 => 'warlock',
                  9 => 'warrior');
+my $counter = 0;
+
+# Load the card scores.
+for my $file (@files) {
+print "Processing $file...\n";
+    if ($file =~ /ha_data_(\d)_.*.txt$/) {
+        
+        my $class_id = $1;
+        $class_maps{$class_id} = {} if !exists($class_maps{$class_id});
+        my $text = read_file($file);
+        my $data = decode_json $text;
+        for my $result (@{$data->{results}}) {
+            print Dumper($result) if $debug >= 3;
+            my $dat = $result->{card};
+            $dat->{name} = lc($dat->{name});
+            $class_maps{$class_id}->{$dat->{name}} = int($dat->{score}*100);
+        }
+    }
+}
+
+# Load the card data.
+my $card_data_text = read_file($card_data_file);
+my $cards = decode_json $card_data_text;
+
+my @sets_to_load = ('Basic', 'Classic', 'Curse of Naxxramas', 'Goblins vs Gnomes');
 for my $class_id (keys(%class_ids)) {
 
     my $class = $class_ids{$class_id};
@@ -45,9 +69,9 @@ for my $class_id (keys(%class_ids)) {
             if (($card->{'id'} =~ /^..._...$/ || $card->{'id'} =~ /^NEW1_...$/ || $card->{'id'} =~ /^tt_...$/) && $card->{'type'} ne 'Hero Power' && $card->{'collectible'}) {    
                 print "Processing: ",$card->{'name'}, ' ', $card->{'id'}, ", #$counter\n";
                 
-                print 'Text: ' . $card->{text} . "\n" if exists($card->{text}) and $debug >= 2;
-                print Dumper($card) if $debug >= 2;
-                print Dumper($card->{mechanics}) if exists($card->{mechanics}) and $debug >= 2;            
+                print 'Text: ' . $card->{text} . "\n" if exists($card->{text}) and $debug >= 3;
+                print Dumper($card) if $debug >= 3;
+                print Dumper($card->{mechanics}) if exists($card->{mechanics}) and $debug >= 3;            
                 
                 my $playerclass = exists($card->{'playerClass'}) ? lc($card->{'playerClass'}) : 'neutral';
                 my $mechanics = $card->{'mechanics'};
@@ -67,6 +91,7 @@ for my $class_id (keys(%class_ids)) {
                                 playerclass => $playerclass,
                                 mechanics => \@mech,
                                 synergy => {},
+                                score => $class_maps{$class_id}->{lc($card->{'name'})},
                             }) if $playerclass eq $class or $playerclass eq 'neutral';
                 $counter += 1;
             }
