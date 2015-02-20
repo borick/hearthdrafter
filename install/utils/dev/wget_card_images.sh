1046159883bc0c758616277b22b8c1ddd9abcaa1
Boris Sitsker <boris@sitsker.com>
2015-02-20 00:07:00 -0500

    more card images.

--- install/utils/dev/wget_card_images.sh
+++ install/utils/dev/wget_card_images.sh
@@ -1,2 +1,7 @@
 BASEURL="http://wow.zamimg.com/images/hearthstone/cards/enus/original/"
-for x in `cat card_images.txt`; do wget "$BASEURL$x.png" -P ../../../public/images/cards; done
+BASEURL2="http://wow.zamimg.com/images/hearthstone/cards/enus/medium/"
+BASEURL3="http://wow.zamimg.com/images/hearthstone/cards/enus/small/"
+#for x in `cat card_images.txt`; do wget "$BASEURL$x.png" -P ../../../public/images/cards; done
+for x in `cat card_images.txt`; do wget "$BASEURL2$x.png" -P ../../../public/images/cards_medium; done
+for x in `cat card_images.txt`; do wget "$BASEURL3$x.png" -P ../../../public/images/cards_small; done
+
