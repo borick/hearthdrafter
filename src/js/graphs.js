d2bb9766d4e64e2632ea2747eca0ca9fd1b35890
Boris Sitsker <boris@sitsker.com>
2015-05-16 23:42:49 -0400

    fix labels

--- src/js/graphs.js
+++ src/js/graphs.js
@@ -12,8 +12,8 @@ function doBreakdown(drops_array) {
         bindto: "#tabs-3"
     });
     chart.axis.labels({
-    x: 'Number of Drops',
-    y: 'Mana Cost'
+    y: 'Number of Drops',
+    x: 'Mana Cost'
     });
     
     //tab it up
