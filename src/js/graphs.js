94943edb85eb03cf62f17e08153f875bee331653
Boris Sitsker <boris@sitsker.com>
2015-05-16 23:43:47 -0400

    oops

--- src/js/graphs.js
+++ src/js/graphs.js
@@ -12,8 +12,8 @@ function doBreakdown(drops_array) {
         bindto: "#tabs-3"
     });
     chart.axis.labels({
-    y: 'Number of Drops',
-    x: 'Mana Cost'
+    x: 'Number of Drops',
+    y: 'Turn Played'
     });
     
     //tab it up
